c032fe97a38b854d8f008a46e4fcd8afed6a968f
technokowski <gabrielkavaloski@gmail.com>
2018-02-13 21:05:22 -0800

    added login page

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+from src.common.database import Database
 from src.models.user import User
 
 __author__ = "technokowski"
@@ -8,19 +9,26 @@ __author__ = "technokowski"
 from flask import Flask, render_template, request, session
 
 app = Flask(__name__) #__main__
+app.secret_key = 'jose'
 app.debug = True
 
 @app.route('/') # www.mysite.com/api/
 def hello_message():
     return render_template('login.html')
 
-@app.route('/login')
+@app.before_first_request
+def initialize_database():
+    Database.initialize()
+
+@app.route('/login', methods=['POST'])
 def login_user():
     email = request.form['email']
     password = request.form['password']
 
     if User.login_valid(email, password):
         User.login(email)
+    else:
+        session['email'] = None
 
     return render_template("profile.html", email=session['email'])
 
