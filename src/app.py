818653a317a4ce8cf6b25c407f04f9406e2cc736
technokowski <gabrielkavaloski@gmail.com>
2018-02-28 07:53:04 -0800

    bootstrap styling added

--- src/app.py
+++ src/app.py
@@ -12,15 +12,23 @@ app = Flask(__name__) #__main__
 app.secret_key = 'jose'
 app.debug = True
 
-@app.route('/') # www.mysite.com/api/
-def hello_message():
+@app.route('/')
+def home_template():
+    return render_template('home.html')
+
+@app.route('/login') # www.mysite.com/api/
+def login_template():
     return render_template('login.html')
+
+@app.route('/register') # www.mysite.com/api/
+def register_template():
+    return render_template('register.html')
 
 @app.before_first_request
 def initialize_database():
     Database.initialize()
 
-@app.route('/login', methods=['POST'])
+@app.route('/auth/login', methods=['POST'])
 def login_user():
     email = request.form['email']
     password = request.form['password']
@@ -29,6 +37,15 @@ def login_user():
         User.login(email)
     else:
         session['email'] = None
+
+    return render_template("profile.html", email=session['email'])
+
+@app.route('/auth/register', methods=['POST'])
+def register_user():
+    email = request.form['email']
+    password = request.form['password']
+
+    User.register(email, password)
 
     return render_template("profile.html", email=session['email'])
 
